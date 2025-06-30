8adbcdb3ec3dc30eed39d46ccf1f4ac4a3250cd6
Shubham  Sarode <86552457+shubh435@users.noreply.github.com>
2025-06-30 08:49:37 +0530

    Create first letter remove .js

---
+++ extras/firstletterremove.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * 4. How Remove the first element from an array in JavaScript?
+ */
+
+const arr= [1,8,3,4,5,6];
+const removeFirst = (arr) => {
+    return arr.filter((ele,index)=>index!=0)
+}
+
+console.log(removeFirst(arr))
